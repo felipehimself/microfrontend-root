6e13bbf1900ecf2780efef50600189e6451078df
Felype Couto <felype_rocha@ecad.org.br>
2024-05-17 22:41:12 -0300

    feat: added new route and implemented way to pass the mfe routes to the mfe navbar

--- src/microfrontend-root-root-config.ts
+++ src/microfrontend-root-root-config.ts
@@ -1,18 +1,14 @@
-import { registerApplication, start } from 'single-spa';
+import { registerApplication, start } from "single-spa";
 import {
   constructApplications,
   constructLayoutEngine,
   constructRoutes,
-} from 'single-spa-layout';
-import microfrontendLayout from './microfrontend-layout.html';
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
 
 async function boostrap() {
-  // const navbar = await System.import("@microfrontend-navbar");
-  // registerApplication({
-  //   name: "@microfrontend-navbar",
-  //   app: navbar.default,
-  //   activeWhen: ["/"],
-  // });
+  const navbarChannel = new BroadcastChannel("navbarChannel");
+  const navbarRoutes = [];
 
   const routes = constructRoutes(microfrontendLayout);
   const applications = constructApplications({
@@ -27,7 +23,53 @@ async function boostrap() {
     registerApplication({ ...app, customProps: {} })
   );
 
-  await Promise.all(['@mfe-lib/styleguide', '@microfrontend-navbar']);
+  await Promise.all(["@mfe-lib/styleguide", "@microfrontend-navbar"]);
+
+  (async function () {
+    for (const [_, app] of applications.entries()) {
+      try {
+        const module = await System.import(app.name);
+        if (module.navbarRoutes) {
+          navbarRoutes.push(module.navbarRoutes);
+
+          const teste = {
+            appName: "Reports",
+            appIcon: "/streaming/streaming-nav-icon.svg",
+            routes: [
+              {
+                menuName: "General",
+                menuChildren: [
+                  {
+                    submenuName: "General",
+                    path: "stock/stock/stock",
+                  },
+                ],
+              },
+              {
+                menuName: "Internal",
+                menuChildren: [
+                  {
+                    submenuName: "Internal",
+                    path: "Sales/Sales/Sales",
+                  },
+                ],
+              },
+            ],
+          };
+          navbarRoutes.push(teste);
+        }
+      } catch (error) {
+        console.log(error);
+        const [_, erroUrl] = error?.message?.split("//");
+        const [hostname] = erroUrl?.split(".");
+        const idx = hostname?.lastIndexOf("-");
+        const appName = hostname?.substring(0, idx);
+        // loadAppsErros.push(appName);
+      }
+    }
+  })().then(() => {
+    navbarChannel.postMessage(navbarRoutes);
+  });
 
   layoutEngine.activate();
   start();
