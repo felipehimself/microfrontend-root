67e99a5bdd4502223a41c00d76e2b2d834f9a18f
Felype Couto <felype_rocha@ecad.org.br>
2024-05-18 21:51:11 -0300

    feat: added new routes and handling routes coming from another mfe to dispatch to mfe navbar

--- src/microfrontend-root-root-config.ts
+++ src/microfrontend-root-root-config.ts
@@ -23,40 +23,15 @@ async function boostrap() {
     registerApplication({ ...app, customProps: {} })
   );
 
-  await Promise.all(["@mfe-lib/styleguide", "@microfrontend-navbar"]);
+  // await Promise.all(["@mfe-lib/styleguide", "@microfrontend-navbar"]);
 
   (async function () {
     for (const [_, app] of applications.entries()) {
       try {
         const module = await System.import(app.name);
-        if (module.navbarRoutes) {
-          navbarRoutes.push(module.navbarRoutes);
 
-          const teste = {
-            appName: "Reports",
-            appIcon: "/streaming/streaming-nav-icon.svg",
-            routes: [
-              {
-                menuName: "General",
-                menuChildren: [
-                  {
-                    submenuName: "General",
-                    path: "stock/stock/stock",
-                  },
-                ],
-              },
-              {
-                menuName: "Internal",
-                menuChildren: [
-                  {
-                    submenuName: "Internal",
-                    path: "Sales/Sales/Sales",
-                  },
-                ],
-              },
-            ],
-          };
-          navbarRoutes.push(teste);
+        if (module.routes) {
+          navbarRoutes.push(module.routes);
         }
       } catch (error) {
         console.log(error);
