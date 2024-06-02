1adaac8035591dc28628b68170c6d36b2cd2a1e5
felipe <felipecoutodarocha@gmail.com>
2024-06-02 11:09:50 -0300

    feat: added logic to control errors whie loading the mfes

--- src/microfrontend-root-root-config.ts
+++ src/microfrontend-root-root-config.ts
@@ -1,14 +1,25 @@
-import { registerApplication, start } from "single-spa";
+import {
+  LOAD_ERROR,
+  addErrorHandler,
+  getAppStatus,
+  navigateToUrl,
+  registerApplication,
+  start,
+  unloadApplication,
+} from 'single-spa';
 import {
   constructApplications,
   constructLayoutEngine,
   constructRoutes,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
+} from 'single-spa-layout';
+import microfrontendLayout from './microfrontend-layout.html';
 
 async function boostrap() {
-  const navbarChannel = new BroadcastChannel("navbarChannel");
+  const navbarChannel = new BroadcastChannel('navbarChannel');
+  // const appErrorsChannel =  new BroadcastChannel("appErrorsChannel");
+
   const navbarRoutes = [];
+  const appErrors = [];
 
   const routes = constructRoutes(microfrontendLayout);
   const applications = constructApplications({
@@ -35,18 +46,35 @@ async function boostrap() {
         }
       } catch (error) {
         console.log(error);
-        const [_, erroUrl] = error?.message?.split("//");
-        const [hostname] = erroUrl?.split(".");
-        const idx = hostname?.lastIndexOf("-");
+        const [_, erroUrl] = error?.message?.split('//');
+        const [hostname] = erroUrl?.split('.');
+        const idx = hostname?.lastIndexOf('-');
         const appName = hostname?.substring(0, idx);
-        // loadAppsErros.push(appName);
+        console.log('Error loading: ', appName);
+        // appErrors.push(appName);
       }
     }
   })().then(() => {
     navbarChannel.postMessage(navbarRoutes);
+    // In case you want to show the errors in the UI (e.g. navbar)
+    // appErrorsChannel.postMessage(appErrors);
   });
 
   layoutEngine.activate();
+
+  addErrorHandler(async (err) => {
+    console.log(`Erro SingleSpa ${err.appOrParcelName}: ${err}`);
+
+    if (getAppStatus(err.appOrParcelName) === LOAD_ERROR) {
+      System.delete(System.resolve(err.appOrParcelName));
+
+      setTimeout(() => {
+        navigateToUrl(`/not-found?app=${err.appOrParcelName}`);
+        unloadApplication(err.appOrParcelName);
+      }, 0);
+    }
+  });
+
   start();
 }
 
